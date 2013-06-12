427562dcc500773ac16ee58c8eb59fd6bb1afaf7
Jonathan Perkin <jperkin@smartos.org>
2013-06-12 16:19:53 +0100

    mk/pkginstall: DATASET-780, permit existing users
    There should be an option with packages to re-use an existing user or
    group, even if a specific uid/gid is provided.  This commit introduces a
    user variable to configure this: PKGINSTALL_IGNORE_UIDGID.
    
    When PKGINSTALL_IGNORE_UIDGID is true then pkg_install will not try to
    create the user/group as specified.  If the user/group does not exist
    but the specified uid/gid is, then the uid/gid will be stripped so that
    the system can allocate one at random.

--- mk/pkginstall/bsd.pkginstall.mk
+++ mk/pkginstall/bsd.pkginstall.mk
@@ -11,10 +11,22 @@
 #	noting the actions taken if PKG_UPDATE_FONTS_DB is YES.  It is either
 #	YES or NO and defaults to YES for PKG_DEVELOPERs, otherwise NO.
 #
+# PKGINSTALL_IGNORE_UIDGID
+#	Whether or not to treat the specific uid/gid provided by the package as
+#	simply advisory.  In the case where either the user/group already
+#	exists but the uid/gid does not match, or the uid/gid is already taken
+#	by a different user:
+#
+#	* "yes" will add the user/group if necessary with an unspecified uid/gid
+#	* "no" will not add the user/group and fail the install.
+#
+#	Default value: no
+#
 _VARGROUPS+=		pkginstall
 _USER_VARS.pkginstall= \
 	FONTS_VERBOSE \
 	OCAML_FINDLIB_REGISTER_VERBOSE \
+	PKGINSTALL_IGNORE_UIDGID \
 	PKG_CREATE_USERGROUP \
 	PKG_CONFIG PKG_CONFIG_PERMS \
 	PKG_RCD_SCRIPTS \
@@ -1041,6 +1053,9 @@ FILES_SUBST+=		PKG_REGISTER_SHELLS=${PKG_REGISTER_SHELLS:Q}
 FILES_SUBST+=		PKG_UPDATE_FONTS_DB=${PKG_UPDATE_FONTS_DB:Q}
 FILES_SUBST+=		FONTS_VERBOSE=${FONTS_VERBOSE:Q}
 FILES_SUBST+=		OCAML_FINDLIB_REGISTER_VERBOSE=${OCAML_FINDLIB_REGISTER_VERBOSE:Q}
+
+PKGINSTALL_IGNORE_UIDGID?=	no
+FILES_SUBST+=			PKGINSTALL_IGNORE_UIDGID=${PKGINSTALL_IGNORE_UIDGID:Q}
 
 # Substitute for various programs used in the DEINSTALL/INSTALL scripts and
 # in the rc.d scripts.
